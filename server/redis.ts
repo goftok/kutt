856ddd9ba66c4125118404d7cba364a2a4ff6849
goftok <skills.lancets0n@icloud.com>
2024-10-14 15:15:17 +0200

    some changes in env

--- server/redis.ts
+++ server/redis.ts
@@ -2,12 +2,7 @@ import Redis from "ioredis";
 
 import env from "./env";
 
-const client = new Redis({
-  host: env.REDIS_HOST,
-  port: env.REDIS_PORT,
-  db: env.REDIS_DB,
-  ...(env.REDIS_PASSWORD && { password: env.REDIS_PASSWORD })
-});
+const client = new Redis(env.REDIS_REDIS_URL);
 
 export default client;
 
