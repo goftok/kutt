1351c24ce24743f52a4dfa459c560f986b57f13c
goftok <skills.lancets0n@icloud.com>
2024-10-14 15:26:29 +0200

    debug

--- server/knex.ts
+++ server/knex.ts
@@ -4,7 +4,15 @@ import env from "./env";
 
 const db = knex({
   client: "postgres",
-  connection: env.DB_URL,
+  connection: {
+    connectionString: env.DB_URL, 
+    ssl: {
+      rejectUnauthorized: false
+    }
+  },
+  pool: {
+    min: env.DB_POOL_MIN || 2, 
+    max: env.DB_POOL_MAX || 10
+  }
 });
-
 export default db;
